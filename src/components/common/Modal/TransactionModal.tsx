1c75a664b33e7bcfdc326542908f68b53d204f93
Arimoro Olamilekan Ezekiel <67231587+Lexitar32@users.noreply.github.com>
2024-05-08 22:17:59 +0100

    transaction form in progress

--- src/components/common/Modal/TransactionModal.tsx
+++ src/components/common/Modal/TransactionModal.tsx
@@ -8,38 +8,46 @@ export const TransactionModal: React.FC = () => {
   const { openModal, setOpenModal } = useModalContext();
 
   return (
-    <Transition
-      show={openModal}
-      enter="transition-opacity duration-300 ease-out"
-      enterFrom="opacity-0"
-      enterTo="opacity-100"
-      leave="transition-opacity duration-300 ease-out"
-      leaveFrom="opacity-100"
-      leaveTo="opacity-0"
-      as={Fragment}
-    >
-      <div className="fixed inset-0 overflow-y-auto z-999999 font-mont">
-        <div className="flex items-center justify-center min-h-screen p-6">
-          <div className="fixed inset-0 opacity-85 bg-boxdark"></div>
+    <>
+      {/* Dark Overlay */}
+      <div
+        className={`fixed inset-0 bg-black opacity-50 z-9999 ${
+          openModal ? "" : "hidden"
+        }`}
+      ></div>
 
-          <div className="relative overflow-hidden transition-transform duration-500 ease-in-out translate-y-[-50px] bg-white rounded-lg shadow-xl min-w-96">
-            <div className="flex items-center justify-between px-4 py-3">
-              <h2 className="text-lg font-semibold">New transaction</h2>
-              <CancelIcon setOpen={setOpenModal} />
-            </div>
-            <div className="p-4">
-              <p className="text-sm">
-                Record your income or expenses with ease &#8208; add a new
-                transaction now!
-              </p>
+      {/* Modal Content */}
+      <Transition
+        show={openModal}
+        enter="transform transition-transform duration-300 ease-in-out"
+        enterFrom="scale-0"
+        enterTo="scale-100"
+        leave="transform transition-transform duration-300 ease-in-out"
+        leaveFrom="scale-100"
+        leaveTo="scale-0"
+        as={Fragment}
+      >
+        <div className="fixed inset-0 overflow-y-auto z-999999 font-mont">
+          <div className="flex items-center justify-center min-h-screen p-6">
+            <div className="relative overflow-hidden bg-white rounded-lg shadow-xl min-w-96">
+              <div className="flex items-center justify-between px-4 py-3">
+                <h2 className="text-lg font-semibold">New transaction</h2>
+                <CancelIcon setOpen={setOpenModal} />
+              </div>
+              <div className="p-4">
+                <p className="text-sm">
+                  Record your income or expenses with ease &#8208; add a new
+                  transaction now!
+                </p>
 
-              <div className="mt-5">
-                <TransactionForm />
+                <div className="mt-5">
+                  <TransactionForm />
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </Transition>
+      </Transition>
+    </>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -42,6 +42,7 @@ module.exports = {
       "meta-7": "#FF6766",
       "meta-8": "#F0950C",
       "meta-9": "#E5E7EB",
+      modalBg: "rgba(0, 0, 0, .7)",
       success: "#219653",
       danger: "#D34053",
       warning: "#FFA70B",
